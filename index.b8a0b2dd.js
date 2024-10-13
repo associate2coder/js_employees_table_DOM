c8f47f71b094c457f19abccf1e3451a0e0f339d9
associate2coder <olatsko@gmail.com>
2024-10-13 13:14:16 +0300

    make build

---
+++ index.b8a0b2dd.js
@@ -0,0 +1,2 @@
+window.convertNumberToSalary=function(n){return"$".concat((+n).toLocaleString("en-US"))},window.convertSalaryToNumber=function(n){return Number(n.substring(1).split(",").join(""))},window.getValueByCellIndex=function(n,t){return n.cells[t].textContent};
+//# sourceMappingURL=index.b8a0b2dd.js.map
